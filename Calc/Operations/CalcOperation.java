507b2cae686bf8aa151a2cf3329ab65594d87beb
Nikita <silvernikchuprov@gmail.com>
2023-06-18 12:43:56 +0300

    Homework6 update

---
+++ Calc/Operations/CalcOperation.java
@@ -0,0 +1,45 @@
+package Calc.Operations;
+
+import Calc.CalcModel;
+
+public class CalcOperation implements MultDiv, PlusMinus{
+    public double goOperation(double x, double y, char operation){
+        switch (operation) {
+            case '+' -> {
+                return sum(x, y);
+            }
+            case '-' -> {
+                return sub(x, y);
+            }
+            case '*' -> {
+                return multiply(x, y);
+            }
+            case '/' -> {
+                return divide(x, y);
+            }
+            default -> {
+                return 0;
+            }
+        }
+    }
+
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+
+    public double divide(double a, double b) {
+        return a / b;
+    }
+
+
+    public double sum(double a, double b) {
+        return a + b;
+    }
+
+
+    public double sub(double a, double b) {
+        return a - b;
+    }
+}
